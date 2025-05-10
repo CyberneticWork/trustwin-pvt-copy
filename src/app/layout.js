353d06e46c11acd163837290e5c6a985ce2099fe
thechandii <87866209+thechandii@users.noreply.github.com>
2025-05-11 00:33:19 +0530

    LKR-ACL

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,23 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ChildRender from "@/components/main/ChildHandling";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "TrustWin | Loan Management System",
-  description: "We provide best financial solutions and services to the customers through building strong relationships as a trusted friend to enhancing their quality of life.",
+  description:
+    "We provide best financial solutions and services to the customers through building strong relationships as a trusted friend to enhancing their quality of life.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="manifest" href="/manifest.json" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased font-sf">
         <ChildRender>{children}</ChildRender>
       </body>
     </html>
